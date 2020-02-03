4c2bcddfbf324ec8e940e982263bebc610656e3a
Alejandro Lopez <58063303+Alejandro-HUB@users.noreply.github.com>
2020-02-03 09:54:39 -0500

    Add files via upload

---
+++ CatalinaChatBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=catalinabot-bot-Web-Deploy.pubxml -p:Password=Nxh2CzsTZha39FWK0c9vRxdnM1g5uMehlywr9mv1jYj6XQ6HtHpKulogjHrl
+
